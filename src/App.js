5137d965ee601ae98a5c93c79a3137c31807a66a
Sanchari <95698881+Sanchari1999@users.noreply.github.com>
2023-04-25 09:05:46 +0000

    Update 5 files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Editor from './Editor';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Editor />
+    </div>
+  );
+};
+
+export default App;
